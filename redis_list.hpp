e9fa448182d67ece97231fae3d3117fd8cba8dec
billionairiam <75885714+billionairiam@users.noreply.github.com>
2021-09-01 13:35:48 +0800

    Update redis_list.hpp

--- redis_list.hpp
+++ redis_list.hpp
@@ -1,1 +1,17 @@
-#include<>
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct listNode{
+  listNode*prev;
+  listNode*next;
+  void* val;
+}listNode;
+
+typedef struct list{
+  listNode*head;
+  listNode*tail;
+  unsigned long len;
+  void*(*dup)(void* ptr);
+  void*(*free)(void* ptr);
+  int (*match)(void *ptr,void *key);
+};
